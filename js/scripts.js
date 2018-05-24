4aa6323d0197d9d8222ecb2031f8be8eea95f2b1
Stuart Spence <stuart.a.spence@gmail.com>
2018-05-24 00:02:02 -0400

    reads google calendar to populate events

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,89 @@
  "use strict";
 
-$(document).ready(function(){
-    
-		$('ul.thumbnail li').hover(function() {
-			$(this).find('span').fadeToggle('fast');
+var urlPattern=/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/;
+
+function refreshEventList(){
+	console.log("Refreshing event list.");
+	
+	var url="https://www.googleapis.com/calendar/v3/calendars/shyshyschullie@gmail.com/events";
+	url+="?key=AIzaSyCDyJdWjIEO_PIxuEC7zdz64sg69hFHfzs&timeMin=";
+	url+=moment().subtract(24,"hours").format("YYYY-MM-DDTHH:mm:ss")+"Z";
+	
+	$.get(url,
+	      function(data){
+		console.log("Successfully retrieved Calendar API data.")
+		data.items.sort(function(a,b){
+			var aDate="dateTime" in a.start?a.start.dateTime:a.start.date;
+			var bDate="dateTime" in b.start?b.start.dateTime:b.start.date;
+			return aDate>bDate;
 		});
-        
-		$('.newsletter-button').hover(function() {
-			$(this).fadeToggle('fast');
-		});        
+		for (var index=0;index<data.items.length;index++){
+			var event=data.items[index];
+			processEventData(event);
+		}		
+	      })
+}
+
+function processEventData(event){
+	//Process one event item from the JSON array returned by the Google Calendar API request.
+	
+	try{
+		var title=event.summary;
+		var url = urlPattern.exec(event.description);
+		url=url?url[0]:"";
+		
+		var d = "dateTime" in event.start?event.start.dateTime:event.start.date;
+		var dateText=moment(d).format("dddd, MMMM Do");
+		
+		var details="";	
+		if("dateTime" in event.start){
+			details=moment(d).format("h:mm a")+" to "+moment(event.end.dateTime).format("h:mm a");
+		}
+		
+		addEvent(dateText,title,details,url);
+	}catch(error){
+		console.error("processEventData failed.");
+		console.error(JSON.stringify(event,null,2));
+		console.error(error);
+	}
+}
+
+function addEvent(dateText,title,details,url){
+	var event=getNewEventElement("#event-template");
+	setEventElement(event,dateText,title,details,url);
+}
+
+function setEventElement(event,dateText,title,details,url){
+	$(event).find(".event-date").html(dateText);
+	$(event).find(".event-title").html(title);
+	$(event).find(".event-details").html(details);
+	
+	var urlButton=$(event).find(".event-url");
+	if(url){
+		urlButton.attr("href",url);
+	}else{
+		urlButton.remove();
+	}
+	$("#events-container").append(event);
+}
+
+function getNewEventElement(query){
+	var clone = $(query).clone(true,true);
+	if (clone.length != 1){
+		console.error("Uh oh! getEventClone query did not find exactly one element. query = '"+query+"'");
+	}
+	clone.removeAttr("id");
+	return clone;
+}
+
+$(document).ready(function(){
+	refreshEventList();
+    
+	$('ul.thumbnail li').hover(function() {
+		$(this).find('span').fadeToggle('fast');
+	});
+	
+	$('.newsletter-button').hover(function() {
+		$(this).fadeToggle('fast');
+	});
 });
